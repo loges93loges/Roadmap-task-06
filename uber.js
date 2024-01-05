0cbe4dda4a38d41cbaa6e792cf9e160ceb0d01bc
Loges k <154902410+loges93loges@users.noreply.github.com>
2024-01-05 14:08:12 +0530

    uber.js

--- uber.js
+++ uber.js
@@ -4,13 +4,13 @@ class UberCalculator {
         this.costPerMile = costPerMile;
         this.costPerMinute = costPerMinute;
     }
-    calculateFare(distance, time) {
+    calculateprice(distance, time) {
         const Price = this.baseFare + (distance * this.costPerMile) + (time * this.costPerMinute);
         return Price;
     }
 }
 const uberCalculator1 = new UberCalculator(10, 5, 0.5);
-const fare = uberCalculator1.calculateFare(15, 10);
+const fare = uberCalculator1.calculateprice(15, 10);
 
 
 console.log(`Uber Price: $${fare}`);
